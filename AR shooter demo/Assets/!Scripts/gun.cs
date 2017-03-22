f0244a5939717bced74a7fd3180de0ced9722636
Akshit Arora <akshit.arora1995@gmail.com>
2017-03-22 15:43:46 +0530

    ar shooter demo | initial commit

---
+++ AR shooter demo/Assets/!Scripts/gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class gun : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform spawnObject;	
+
+	void Awake() {
+//		bool focusModeSet = CameraDevice.Instance.SetFocusMode( 
+//			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject go = Instantiate (bulletPrefab, spawnObject.position, spawnObject.rotation) as GameObject;
+			//go.transform.parent = transform; // added to make bullet a child of the image target.
+			go.GetComponent<Rigidbody> ().AddForce (transform.forward * 100, ForceMode.VelocityChange);
+		}
+	}
+}
