6bbf73a54cdf0324c6cdf9ee5f7d8aad349d0bae
Akshit Arora <akshit.arora1995@gmail.com>
2017-03-23 17:32:08 +0530

    sprint_23mar_1
    crosshairs, soldier die on collision, gun triggers, colliders, raycasts

---
+++ sprint_23mar/Assets/!Scripts/enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter (Collision collision){
+		Debug.Log ("collided!");
+		Destroy (collision.gameObject);
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
