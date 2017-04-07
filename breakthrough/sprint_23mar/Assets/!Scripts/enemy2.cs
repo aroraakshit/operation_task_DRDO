b9dcbac0b1a20e2fc67a287cbd1588133da7e1a6
Akshit Arora <akshit.arora1995@gmail.com>
2017-04-07 17:35:20 +0530

    implemented finite state machine for enemy soldier and defined boundaries for the soldier

--- breakthrough/sprint_23mar/Assets/!Scripts/enemy2.cs
+++ breakthrough/sprint_23mar/Assets/!Scripts/enemy2.cs
@@ -12,24 +12,61 @@ public class enemy2 : MonoBehaviour{
 	public GameObject plan;
 	public Transform target;
 	public int teleport;
+	public string[] p_status = {"WALK_STRAIGHT", "TURN_LEFT", "TURN_RIGHT", "STOP"};
+	public int status;
+	GameObject boundary;
 
 	// Use this for initialization
 	void Start () {
+		status = Random.Range (0,3);
 		canvas = GameObject.FindGameObjectWithTag ("canvas_text");
 		notice = GameObject.FindGameObjectWithTag ("notice");
 		plan = GameObject.FindGameObjectWithTag ("imgtgt");
+		boundary = GameObject.FindGameObjectWithTag ("boundary");
 		target = GameObject.FindGameObjectWithTag ("MainCamera").transform;
-
 		teleport = plan.GetComponent<gameController> ().newLocation ();;
 		Debug.Log ("Selected location number: " + teleport.ToString());
 		GameObject[] spe = GameObject.FindGameObjectsWithTag ("enemyspawn");
 		gameObject.transform.position = spe [teleport].transform.position;
+		StartCoroutine (fsm (2));
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (boundary.transform.position.z - gameObject.transform.position.z <= -15 || boundary.transform.position.z - gameObject.transform.position.z >= 15 || boundary.transform.position.x - gameObject.transform.position.x <= -15 || boundary.transform.position.x - gameObject.transform.position.x >= 15) {
+			transform.Rotate(0,180,0);
+			Debug.Log ("Boundary hit!");
+		}
 
-		gameObject.transform.LookAt (target);
+		switch (status) {
+		case 0:
+			{
+				//code for walk straight
+				transform.position += (transform.forward * Time.deltaTime * 10);
+				break;
+			}
+		case 1:
+			{
+				//code for turn left
+				transform.Rotate(0,-90 * Time.deltaTime,0);
+				status = 0;
+				break;
+			}
+		case 2:
+			{
+				//code for turn right
+				transform.Rotate(0,90,0);
+				status = 0;
+				break;
+			}
+		case 3:
+			{
+				//code for stop
+				break;
+			}
+		default:
+			break;
+		}
 
 		if (health == 0) {
 			plan.GetComponent<gameController> ().createEnemy (teleport);
@@ -38,9 +75,7 @@ public class enemy2 : MonoBehaviour{
 			Destroy (gameObject);
 		}
 
-//		if (target != null) {
-//			transform.LookAt(target);
-//		}
+		//gameObject.transform.LookAt (target);
 
 	}
 
@@ -58,6 +93,64 @@ public class enemy2 : MonoBehaviour{
 		if (collision.collider.name == "bullet(Clone)") {
 			decreaseHealth ("injured");
 		}
+
 	}
 
+	IEnumerator fsm (int samplingRate) {
+		//finite state machine goes here
+		while (true) {
+			changeStatus ();
+			//Debug.Log (gameObject.name + " is now " + p_status[status]);
+			yield return new WaitForSeconds (samplingRate);
+		}
+	}
+
+	void changeStatus () {
+		float rnd = Random.value;
+		if (status == 0) {
+			//previous state running, next state?
+			if (rnd <= 0.2) {
+				status = 0;
+			} else if (rnd <= 0.5) {
+				status = 1;
+			} else if (rnd <= 0.7) {
+				status = 2;
+			} else {
+				status = 3;
+			}
+		} else if (status == 1) {
+			//previous state turn left, next state?
+			if (rnd <= 0.2) {
+				status = 0;
+			} else if (rnd <= 0.5) {
+				status = 1;
+			} else if (rnd <= 0.7) {
+				status = 2;
+			} else {
+				status = 3;
+			}
+		} else if (status == 2) {
+			//previous state turn right, next state?
+			if (rnd <= 0.2) {
+				status = 0;
+			} else if (rnd <= 0.5) {
+				status = 1;
+			} else if (rnd <= 0.7) {
+				status = 2;
+			} else {
+				status = 3;
+			}
+		} else {
+			//previous state stop, next state?
+			if (rnd <= 0.2) {
+				status = 0;
+			} else if (rnd <= 0.5) {
+				status = 1;
+			} else if (rnd <= 0.7) {
+				status = 2;
+			} else {
+				status = 3;
+			}
+		}
+	}
 }
